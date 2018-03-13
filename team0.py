7ab772f5841dd9c807c005ccc2d88590a9643fb2
vickikim <31217495+vickikim@users.noreply.github.com>
2018-03-13 09:06:59 -0700

    Final

--- team0.py
+++ team0.py
@@ -15,19 +15,44 @@ def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
+    team_name = 'CopyCat'
+team_strategy =  'Randomness and Luck'
+strategy_description = 'Start randomly and confuse the oppt. Then copy their move every other other. Everything else, betray,'
+
+
+def move(my_history, their_history, my_score, their_score):
+    ''' Arguments accepted: my_history, their_history are strings.
+    my_score, their_score are ints.
+
     Make my move.
     Returns 'c' or 'b'. 
     '''
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
+    # their_history: a string of the same length as history, possibly empty.
     # The first round between these two players is my_history[0] and their_history[0].
     # The most recent round is my_history[-1] and their_history[-1].
-    
+
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return c
+
+    if len(my_history) == 0 or len(my_history) == 1:
+        return 'b'
+        # check if they betray in the first 4 turns. If they did
+        # we will betray, otherwise collude
+    #have something for turns 3 & 4 when len is 2 & 3
+    if len(their_history) == 2 and 3:
+        return 'c'
+    if 'b' == their_history[0] or 'b' == their_history[1]:
+        return 'b'
+    # copy the opponent
+    if their_history[-1] == 'b' or 'c':
+        return their_history[-1]
+    if their_history[-2:-1] == 'c':
+        return 'c'
+    else:
+        return 'c'
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
