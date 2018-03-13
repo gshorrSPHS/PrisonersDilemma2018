86178982b08ec3a982a8bbb8e46d1e2cd86f0df4
AngelRomero2526 <31216888+AngelRomero2526@users.noreply.github.com>
2018-03-13 09:21:18 -0700

    Update and rename team0.py to team3A.py

--- team3A.py
+++ team3A.py
@@ -1,15 +1,11 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
+#     team_name = '3 / Side B'
+#     strategy_name = 'The JUKE'
+#     strategy_description = 'pick b the first 100, then c the next 100'
 #     move: A function that returns 'c' or 'b'
 ####
-import random 
-
-team_name = 'sample' # Only 10 chars displayed.
-strategy_name = 'always collude'
-strategy_description = 'Picks c always'
+import random
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -18,17 +14,14 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return c
-
+    if len(my_history) == 0:
+        return 'c'
+    elif len(their_history) > 100:
+        if len(theiry_history) % 100 == 200:
+            return 'b'
+        elif len(their_history) % 200 = 200
+            return 'c'
+    return their_history[5]
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
