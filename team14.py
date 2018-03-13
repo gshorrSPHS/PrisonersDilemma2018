957bb14a950498a7742c806abdc2abef95a1c85d
JoKoyama <31217152+JoKoyama@users.noreply.github.com>
2018-03-13 08:29:13 -0700

    Add files via upload

---
+++ team14.py
@@ -0,0 +1,17 @@
+#     team_name: team 14
+#     strategy_name: reactor b
+#     strategy_description: depending on the umber of betrays this will do the same
+
+def move(their_history, my_history, my_score, their_score):
+    comply = False
+    if 'c' in my_history[-1:] and 'c' not in my_history[-2:-1]:
+        comply = True
+    if comply:
+        return 'c'
+    if 'b' not in their_history:
+        return 'c'
+    if their_history.count('b'):
+        if their_history.count('b') == my_history.count('b'):
+            return 'c'
+        if their_history.count('b') >= my_history.count('b'):
+            return 'b'
