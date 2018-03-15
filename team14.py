e8b75251d980e57b84e48b45f05e6f37173e2e3a
JoKoyama <31217152+JoKoyama@users.noreply.github.com>
2018-03-15 08:21:50 -0700

    Update team14.py

--- team14.py
+++ team14.py
@@ -1,9 +1,11 @@
-#     team_name: team 14
-#     strategy_name: reactor b
-#     strategy_description: depending on the umber of betrays this will do the same
+     team_name: team 14
+     strategy_name: reactor b
+     strategy_description: depending on the umber of betrays this will do the same
 
 def move(their_history, my_history, my_score, their_score):
     comply = False
+    if 'c'not in their_history and their_history >= 1:
+        return 'b'
     if 'c' in my_history[-1:] and 'c' not in my_history[-2:-1]:
         comply = True
     if comply:
